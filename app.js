59a7ea4c53fe89feb886bf28e947f24e30ea4c51
kylerschin <kylerschin@gmail.com>
2016-10-26 08:39:07 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 var crypto = require('crypto-js');
 var generatePassword = require('password-generator');
+var Checker = require('password-checker');
+var checker = new Checker();
+
+function passwordPut() {
+  generatePassword(30, false);
+}
